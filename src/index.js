9b8f9d9c55b52e01bec39e04be6360e30d8bb505
MohammadsaberMirzaei <137197498+MohammadSaberMi@users.noreply.github.com>
2024-04-04 00:33:29 +0330

    debuge header

--- src/index.js
+++ src/index.js
@@ -10,23 +10,3 @@ document.addEventListener('DOMContentLoaded', function () {
     contentBlock1.classList.toggle('hidden');
   });
 });
-
-//document.addEventListener('DOMContentLoaded', function () {
-//  const toggleBtn = document.getElementById('toggleBtn');
-//  const contentBlock = document.getElementById('contentBlock');
-//
-//  const isHidden = localStorage.getItem('isHidden');
-//  if (isHidden !== null) {
-//    if (isHidden === 'true') {
-//      contentBlock.classList.add('hidden');
-//    } else {
-//      contentBlock.classList.remove('hidden');
-//    }
-//  }
-//
-//  toggleBtn.addEventListener('click', function () {
-//    contentBlock.classList.toggle('hidden');
-//    const currentState = contentBlock.classList.contains('hidden');
-//    localStorage.setItem('isHidden', currentState.toString());
-//  });
-//});
